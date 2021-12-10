af5101c5386aa23ffbff9f8d29a99f367e1dcb3d
adrdroid <vians.killms@gmail.com>
2021-12-10 16:30:35 +0700

    Add Swiper Pagination

--- assets/js/script.js
+++ assets/js/script.js
@@ -9,7 +9,31 @@ var hours = ["Sunday, 09.00 – 21.00", "Monday, 09.00 – 21.00", "Tuesday, 09.
 var todaysHours = hours[dayOfWeek];
 document.getElementById("hours").innerHTML = todaysHours;
 
-// AOS JS
+// Swiper
+var swiper = new Swiper(".mySwiper", {
+  slidesPerView: 1,
+  spaceBetween: 10,
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+  },
+  breakpoints: {
+    640: {
+      slidesPerView: 2,
+      spaceBetween: 20,
+    },
+    768: {
+      slidesPerView: 4,
+      spaceBetween: 40,
+    },
+    1024: {
+      slidesPerView: 5,
+      spaceBetween: 50,
+    },
+  },
+});
+
+// AOS
 const galleryImage = document.querySelectorAll(".gallery-card");
 
 galleryImage.forEach((img, i) => {
